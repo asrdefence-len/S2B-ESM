8d42361bcc73b7c143561626a30abfba30edbf09
asrdefence-len <len.sciacca@asrdefence.com>
2026-09-05 13:17:20 +1000

    Configure two simulated radar emitters

--- config.py
+++ config.py
@@ -1,12 +1,29 @@
 # config.py
+
 SAMPLE_RATE_HZ = 40_000_000
 CENTER_FREQUENCY_HZ = 1_000_000_000
-SIM_IF_FREQUENCY_HZ = 2_000_000
-SIM_PULSE_WIDTH_S = 5e-6
-SIM_PRI_S = 1e-3
-SIM_NUM_PULSES = 10
-SIM_AMPLITUDE = 0.5
 SIM_NOISE_STD = 0.02
+
 DETECTION_THRESHOLD = 0.10
 MIN_PULSE_WIDTH_S = 0.5e-6
-SIM_START_DELAY_S = 200e-6
+
+SIM_EMITTERS = [
+    {
+        "name": "Radar A",
+        "if_frequency_hz": 2_000_000,
+        "pulse_width_s": 5e-6,
+        "pri_s": 1e-3,
+        "num_pulses": 10,
+        "amplitude": 0.5,
+        "start_delay_s": 200e-6,
+    },
+    {
+        "name": "Radar B",
+        "if_frequency_hz": 6_000_000,
+        "pulse_width_s": 8e-6,
+        "pri_s": 1.3e-3,
+        "num_pulses": 8,
+        "amplitude": 0.35,
+        "start_delay_s": 500e-6,
+    },
+]
